1f8aad597c937162456b786908fd1e0fd7c9d1e4
ms919 <81754336+ms919@users.noreply.github.com>
2023-03-28 23:25:07 +0900

    0.1.5

--- lib/koushien/output.rb
+++ lib/koushien/output.rb
@@ -1,16 +1,9 @@
-require 'optparse'
-
 module Koushien
-  class Command
+  class Output
     attr_accessor :output_dir
     # デフォルト値設定
     def initialize
       @output_dir = 'koushien_outputs'
-    end
-
-    class << self
-      def exec(options)
-      end
     end
   end
 end
